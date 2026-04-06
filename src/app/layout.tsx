b17ebe9aaf4fd14da8b77da9a3724cf62fb7a6c6
MD. Sadat Alam Protik <protik0939@gmail.com>
2026-04-06 23:16:24 +0600

    Newly Created Portfolio First Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,82 +1,33 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Menu from "@/app/(components)/Menu";
-import { ThemeProvider } from "@/app/(components)/ThemeProvider";
-import ThemeToggle from "@/app/(components)/ThemeToggle";
-import MusicToggle from "@/app/(components)/MusicToggle";
-import { LanguageProvider } from "@/app/(components)/LanguageProvider";
-import LanguageSwitcher from "@/app/(components)/LanguageSwitcher";
-import Preloader from "@/app/(components)/Preloader";
-import { Baloo_Da_2, Hind_Siliguri } from 'next/font/google'
-import Head from "next/head";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: "Home | Sadat Alam Protik",
-  verification: { google: "eIF-DBTlpUgbA88_sV07lhJiT8iX_luc8VqUGSfG8lc" },
-  description: "Oh, oh woe-oh-woah is me 🖤😉",
-  keywords: "Sadat Alam Protik, portfolio, developer, React, Next.js",
-  openGraph: {
-    title: "Sadat Alam Protik",
-    description: "Portfolio of Sadat Alam Protik",
-    url: "https://protik0939.vercel.app/",
-    siteName: "Sadat's Portfolio",
-    images: [
-      {
-        url: "/protikWOutBg.webp",
-        width: 800,
-        height: 800,
-        alt: "Protik's Portfolio Image",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
+  title: "Sadat Alam Protik",
+  description: "Protik's Personal Portfolio Website",
 };
-
-
-const balooDaTwo = Baloo_Da_2({
-  subsets: ['latin'],
-  weight: ['400'],
-  variable: '--font-baloo-da-2'
-})
-
-const hindShiliguri = Hind_Siliguri({
-  subsets: ['bengali', 'latin'],
-  weight: ['400', '600', '700'],
-  variable: '--font-hind-shiliguri'
-})
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
-    <html lang="en">
-      <Head>
-        <link
-          rel="icon"
-          href="./icon.svg"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-      </Head>
-      <body
-        className={` ${balooDaTwo.variable} ${hindShiliguri.variable} antialiased`} >
-        <ThemeProvider>
-          <LanguageProvider>
-            <Preloader />
-            <div className="flex fixed top-4 pr-4 space-x-4 justify-end w-full z-50">
-              <MusicToggle />
-              <ThemeToggle />
-              <LanguageSwitcher />
-            </div>
-            <Menu />
-            {children}
-          </LanguageProvider>
-        </ThemeProvider>
-      </body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import HomeTopBanner from "@/app/(components)/HomeTopBanner";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <HomeTopBanner />
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p>Something New Loading....</p>
+      <p>Till than, Visit old one - <Link target="_blank" href="https://protik09390.vercel.app" className="text-blue-500 hover:underline">TAP_HERE</Link></p>
     </div>
   );
 }
